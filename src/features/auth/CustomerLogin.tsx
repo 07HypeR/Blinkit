d530a3110a9944f42fd9141eadc7f0c39004c333
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-29 21:14:40 +0530

    CUSTOMER LOG_-IN ADDED

--- src/features/auth/CustomerLogin.tsx
+++ src/features/auth/CustomerLogin.tsx
@@ -1,5 +1,14 @@
-import {View, Text, StyleSheet, Animated, Image} from 'react-native';
-import React, {FC, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Animated,
+  Image,
+  SafeAreaView,
+  Keyboard,
+  Alert,
+} from 'react-native';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import {
   GestureHandlerRootView,
   PanGestureHandler,
@@ -8,9 +17,51 @@ import {
 import CustomSafeAreaView from '@components/global/CustomSafeAreaView';
 import ProductSlider from '@components/login/ProductSlider';
 import {resetAndNavigate} from '@utils/NavigationUtils';
+import CustomText from '@components/ui/CustomText';
+import {Colors, Fonts, lightColors} from '@utils/Constants';
+import CustomInput from '@components/ui/CustomInput';
+import CustomButton from '@components/ui/CustomButton';
+import usekeyboardOffsetHeight from '@utils/useKeyboardOffsetHeight';
+import {RFValue} from 'react-native-responsive-fontsize';
+import LinearGradient from 'react-native-linear-gradient';
+
+const bottomColors = [...lightColors].reverse();
 
 const CustomerLogin: FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [loading, setLoading] = useState(false);
   const [gestureSequence, setGestureSequence] = useState<string[]>([]);
+  const keyboardOffsetHeight = usekeyboardOffsetHeight();
+
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (keyboardOffsetHeight == 0) {
+      Animated.timing(animatedValue, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(animatedValue, {
+        toValue: -keyboardOffsetHeight * 0.84,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [keyboardOffsetHeight]);
+
+  const handleAuth = async () => {
+    Keyboard.dismiss();
+    setLoading(true);
+    try {
+      resetAndNavigate('ProductDashboard');
+    } catch (error) {
+      Alert.alert('Login Failed');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleGesture = ({nativeEvent}: any) => {
     if (nativeEvent.state === State.END) {
@@ -42,16 +93,60 @@ const CustomerLogin: FC = () => {
               bounces={false}
               keyboardDismissMode="on-drag"
               keyboardShouldPersistTaps="handled"
-              contentContainerStyle={styles.subContainer}>
+              contentContainerStyle={styles.subContainer}
+              style={{transform: [{translateY: animatedValue}]}}>
+              <LinearGradient colors={bottomColors} style={styles.gradient} />
               <View style={styles.content}>
                 <Image
                   source={require('@assets/images/logo.png')}
                   style={styles.logo}
                 />
+                <CustomText variant="h2" fontFamily={Fonts.Bold}>
+                  India's last minute app
+                </CustomText>
+                <CustomText
+                  variant="h5"
+                  fontFamily={Fonts.SemiBold}
+                  style={styles.text}>
+                  Log in or sign up
+                </CustomText>
+
+                <CustomInput
+                  onChangeText={text => {
+                    setPhoneNumber(text.slice(0, 10));
+                  }}
+                  onClear={() => setPhoneNumber('')}
+                  value={phoneNumber}
+                  left={
+                    <CustomText
+                      style={styles.phoneText}
+                      variant="h6"
+                      fontFamily={Fonts.SemiBold}>
+                      +91
+                    </CustomText>
+                  }
+                  placeholder="Enter mobile number"
+                  inputMode="numeric"
+                />
+
+                <CustomButton
+                  disabled={phoneNumber.length != 10}
+                  onPress={() => handleAuth()}
+                  loading={loading}
+                  title="Continue"
+                />
               </View>
             </Animated.ScrollView>
           </PanGestureHandler>
         </CustomSafeAreaView>
+
+        <View style={styles.footer}>
+          <SafeAreaView>
+            <CustomText fontSize={RFValue(6)}>
+              By Continuing, you agree to our Terms of Service & Privacy Policy
+            </CustomText>
+          </SafeAreaView>
+        </View>
       </View>
     </GestureHandlerRootView>
   );
@@ -61,11 +156,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  text: {
+    marginTop: 2,
+    marginBottom: 25,
+    opacity: 0.8,
+  },
   subContainer: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
     marginBottom: 20,
+  },
+  phoneText: {
+    marginLeft: 10,
   },
   content: {
     justifyContent: 'center',
@@ -80,6 +183,23 @@ const styles = StyleSheet.create({
     width: 50,
     borderRadius: 20,
     marginVertical: 10,
+  },
+  footer: {
+    borderTopWidth: 0.8,
+    borderColor: Colors.border,
+    paddingBottom: 10,
+    zIndex: 22,
+    position: 'absolute',
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#f8f9fc',
+    width: '100%',
+  },
+  gradient: {
+    paddingTop: 60,
+    width: '100%',
   },
 });
 export default CustomerLogin;
