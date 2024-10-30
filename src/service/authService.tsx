8f8d9d77f39e2a13e6c83ab015603348e0d9207f
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-30 16:38:21 +0530

    CUSTOMER LOG-IN TOKEN ADDED

--- src/service/authService.tsx
+++ src/service/authService.tsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import {BASE_URL} from './config';
 import {tokenStorage} from '@state/storage';
 import {useAuthStore} from '@state/authStore';
+import {resetAndNavigate} from '@utils/NavigationUtils';
+import {appAxios} from './apiInterceptors';
 
 export const customerLogin = async (phone: string) => {
   try {
@@ -10,8 +12,38 @@ export const customerLogin = async (phone: string) => {
     tokenStorage.set('accessToken', accessToken);
     tokenStorage.set('refreshToken', refreshToken);
     const {setUser} = useAuthStore.getState();
-    setUser(response.data.customer);
+    setUser(customer);
   } catch (error) {
     console.log('Login Error', error);
+  }
+};
+
+export const refetchUser = async (setUser: any) => {
+  try {
+    const response = await appAxios.get(`/user`);
+
+    setUser(response.data.user);
+  } catch (error) {
+    console.log('Login Error', error);
+  }
+};
+
+export const refresh_tokens = async () => {
+  try {
+    const refreshToken = tokenStorage.getString('refreshToken');
+    const response = await axios.post(`${BASE_URL}/refresh-token`, {
+      refreshToken,
+    });
+
+    const new_access_token = response.data.accessToken;
+    const new_refresh_token = response.data.refreshToken;
+
+    tokenStorage.set('accessToken', new_access_token);
+    tokenStorage.set('refreshToken', new_refresh_token);
+    return new_access_token;
+  } catch (error) {
+    console.log('REFRESH TOKEN ERROR', error);
+    tokenStorage.clearAll();
+    resetAndNavigate('CustomerLogin');
   }
 };
--- src/service/config.tsx
+++ src/service/config.tsx
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:3000/api';
-export const SOCKET_URL = 'http://localhost:3000/api';
+export const BASE_URL = 'http://10.0.2.2:3000/api';
+export const SOCKET_URL = 'http://10.0.2.2:3000/api';
 
 // USE YOUR NETWORK IP OR HOSTED URL
 // export const BASE_URL = '';
