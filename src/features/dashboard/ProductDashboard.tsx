c2dada9182e79816e27fba2c7406176c221aff9a
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-01 21:16:39 +0530

    ANIMATED NOTIFICATION ADDED

--- src/features/dashboard/ProductDashboard.tsx
+++ src/features/dashboard/ProductDashboard.tsx
@@ -1,14 +1,44 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {useAuthStore} from '@state/authStore';
+import {View, Text, Animated as RNAnimated} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import NoticeAnimation from './NoticeAnimation';
+import {NoticeHeight} from '@utils/Scailing';
+
+const NOTICE_HEIGHT = -(NoticeHeight + 12);
 
 const ProductDashboard = () => {
-  const {user} = useAuthStore();
-  console.log(user);
+  const noticePosition = useRef(new RNAnimated.Value(NOTICE_HEIGHT)).current;
+
+  const slideUp = () => {
+    RNAnimated.timing(noticePosition, {
+      toValue: NOTICE_HEIGHT,
+      duration: 1200,
+      useNativeDriver: false,
+    }).start();
+  };
+  const slideDown = () => {
+    RNAnimated.timing(noticePosition, {
+      toValue: 0,
+      duration: 1200,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  useEffect(() => {
+    slideDown();
+    const timeoutId = setTimeout(() => {
+      slideUp();
+    }, 3500);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <View>
-      <Text>ProductDashboard</Text>
-    </View>
+    <NoticeAnimation noticePosition={noticePosition}>
+      <>
+        <View>
+          <Text>ProductDashboard</Text>
+        </View>
+      </>
+    </NoticeAnimation>
   );
 };
 
