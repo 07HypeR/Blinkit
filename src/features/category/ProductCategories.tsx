b0115b550163d87de399334ca4deafec97db6703
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-06 15:59:47 +0530

    APP COMMIT

--- src/features/category/ProductCategories.tsx
+++ src/features/category/ProductCategories.tsx
@@ -1,12 +1,36 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import React, {FC, useState} from 'react';
+import CustomHeader from '@components/ui/CustomHeader';
 
-const ProductCategories = () => {
+const ProductCategories: FC = () => {
+  const [categories, setCategories] = useState<any[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<any>(null);
+  const [products, setProducts] = useState<any[]>([]);
+  const [categoriesLoading, setCategoriesLoading] = useState<boolean>(true);
+  const [productsLoading, setProductsLoading] = useState<boolean>(false);
+
   return (
-    <View>
-      <Text>ProductCategories</Text>
+    <View style={styles.mainContainer}>
+      <CustomHeader title={selectedCategory?.name || 'Categories'} search />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  subContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default ProductCategories;
