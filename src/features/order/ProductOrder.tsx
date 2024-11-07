0d962fe61a8c75674a2182b04fc3d75a4224d4c8
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-07 22:00:10 +0530

    CHECKOUT ADDED

--- src/features/order/ProductOrder.tsx
+++ src/features/order/ProductOrder.tsx
@@ -1,12 +1,59 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, ScrollView, Image} from 'react-native';
+import React, {FC} from 'react';
+import CustomHeader from '@components/ui/CustomHeader';
+import {Colors, Fonts} from '@utils/Constants';
+import OrderList from './OrderList';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {RFValue} from 'react-native-responsive-fontsize';
+import CustomText from '@components/ui/CustomText';
 
-const ProductOrder = () => {
+const ProductOrder: FC = () => {
   return (
-    <View>
-      <Text>ProductOrder</Text>
+    <View style={styles.container}>
+      <CustomHeader title="Checkout" />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <OrderList />
+
+        <View style={styles.flexRowBetween}>
+          <View style={styles.flexRow}>
+            <Image
+              source={require('@assets/icons/coupon.png')}
+              style={{width: 25, height: 25}}
+            />
+            <CustomText variant="h6" fontFamily={Fonts.SemiBold}>
+              Use Coupons
+            </CustomText>
+          </View>
+          <Icon name="chevron-right" size={RFValue(16)} color={Colors.text} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  scrollContainer: {
+    backgroundColor: Colors.backgroundSecondary,
+    padding: 10,
+    paddingBottom: 250,
+  },
+  flexRowBetween: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    flexDirection: 'row',
+    borderRadius: 15,
+  },
+  flexRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10,
+  },
+});
 
 export default ProductOrder;
