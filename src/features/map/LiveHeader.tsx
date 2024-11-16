6eb9340be99124fda4be4a0094e2687da9d90e4a
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-16 21:28:40 +0530

    LIVE-HEADER ADDED

--- src/features/map/LiveHeader.tsx
+++ src/features/map/LiveHeader.tsx
@@ -1,12 +1,77 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet, Pressable} from 'react-native';
+import React, {FC} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {RFValue} from 'react-native-responsive-fontsize';
+import {useAuthStore} from '@state/authStore';
+import {navigate} from '@utils/NavigationUtils';
+import CustomText from '@components/ui/CustomText';
+import {Fonts} from '@utils/Constants';
 
-const LiveHeader = () => {
+const LiveHeader: FC<{
+  type: 'Customer' | 'Delivery';
+  title: string;
+  secondTitle: string;
+}> = ({title, secondTitle, type}) => {
+  const isCustomer = type === 'Customer';
+
+  const {currentOrder, setCurrentOrder} = useAuthStore();
+
   return (
-    <View>
-      <Text>LiveHeader</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.headerContainer}>
+        <Pressable
+          style={styles.backButton}
+          onPress={() => {
+            if (isCustomer) {
+              navigate('ProductDashboard');
+              if (currentOrder?.status == 'delivered') {
+                setCurrentOrder(null);
+              }
+              return;
+            }
+
+            navigate('DeliveryDashboard');
+          }}>
+          <Icon
+            name="chevron-back"
+            size={RFValue(16)}
+            color={isCustomer ? '#fff' : '#000'}
+          />
+        </Pressable>
+
+        <CustomText
+          variant="h8"
+          fontFamily={Fonts.Medium}
+          style={isCustomer ? styles.titleTextWhite : styles.titleTextBlack}>
+          {title}
+        </CustomText>
+        <CustomText
+          variant="h4"
+          fontFamily={Fonts.SemiBold}
+          style={isCustomer ? styles.titleTextWhite : styles.titleTextBlack}>
+          {secondTitle}
+        </CustomText>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    justifyContent: 'center',
+    paddingVertical: 10,
+    alignItems: 'center',
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+  },
+  titleTextBlack: {
+    color: 'black',
+  },
+  titleTextWhite: {
+    color: 'white',
+  },
+});
 
 export default LiveHeader;
