4be3a026e3095a92acbf7363526fe7a1c13aa9df
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-16 21:06:29 +0530

    LIVE-TRACKING ADDED

--- src/features/map/LiveTracking.tsx
+++ src/features/map/LiveTracking.tsx
@@ -1,12 +1,47 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {useAuthStore} from '@state/authStore';
+import {getOrderById} from '@service/orderService';
+import {Colors} from '@utils/Constants';
+import LiveHeader from './LiveHeader';
 
-const LiveTracking = () => {
+const LiveTracking: FC = () => {
+  const {currentOrder, setCurrentOrder} = useAuthStore();
+
+  const fetchOrderDetails = async () => {
+    const data = await getOrderById(currentOrder?._id as any);
+    setCurrentOrder(data);
+  };
+
+  useEffect(() => {
+    fetchOrderDetails;
+  }, []);
+
+  let msg = 'Packing your order';
+  let time = 'Arriving in 10 minutes';
+  if (currentOrder?.status == 'confirmed') {
+    msg = 'Arriving Soon';
+    time = 'Arriving in 8 minutes';
+  } else if (currentOrder?.status == 'arriving') {
+    msg = 'Order Picked Up';
+    time = 'Arriving in 6 minutes';
+  } else if (currentOrder?.status == 'delivered') {
+    msg = 'Order Delivered';
+    time = 'Fastest Delivery ⚡';
+  }
+
   return (
-    <View>
-      <Text>LiveTracking</Text>
+    <View style={styles.container}>
+      <LiveHeader type="Customer" title={msg} secondTitle={time} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.secondary,
+  },
+});
 
 export default LiveTracking;
