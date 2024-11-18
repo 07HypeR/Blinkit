f2e1c4d37f25025057a922b0d16ee9aaca9c2111
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-18 21:52:51 +0530

    DELIVERY-MAP ADDED

--- src/features/delivery/DeliveryDashboard.tsx
+++ src/features/delivery/DeliveryDashboard.tsx
@@ -1,15 +1,48 @@
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
-import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  RefreshControl,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {Colors} from '@utils/Constants';
 import DeliveryHeader from '@components/delivery/DeliveryHeader';
 import {useAuthStore} from '@state/authStore';
 import TabBar from '@components/delivery/TabBar';
+import {fetchOrders} from '@service/orderService';
+import CustomText from '@components/ui/CustomText';
+import OrderItem from '@components/delivery/OrderItem';
 
 const DeliveryDashboard = () => {
   const {user} = useAuthStore();
   const [selectedTab, setSelectedTab] = useState<'available' | 'delivered'>(
     'available',
   );
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<any[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const renderOrderItem = ({item, index}: any) => {
+    return <OrderItem index={index} item={item} />;
+  };
+
+  const fetchData = async () => {
+    setData([]);
+    setRefreshing(true);
+    setLoading(true);
+    const data = await fetchOrders(selectedTab, user?._id, user?.branch);
+    setData(data);
+    setRefreshing(false);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [selectedTab]);
+
   return (
     <View style={styles.container}>
       <SafeAreaView>
@@ -17,6 +50,32 @@ const DeliveryDashboard = () => {
       </SafeAreaView>
       <View style={styles.subContainer}>
         <TabBar selectedTab={selectedTab} onTabChange={setSelectedTab} />
+        <FlatList
+          data={data}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={async () => await fetchData()}
+            />
+          }
+          ListEmptyComponent={() => {
+            if (loading) {
+              return (
+                <View style={styles.center}>
+                  <ActivityIndicator color={Colors.secondary} size="small" />
+                </View>
+              );
+            }
+            return (
+              <View style={styles.center}>
+                <CustomText>No Orders found yet!</CustomText>
+              </View>
+            );
+          }}
+          renderItem={renderOrderItem}
+          keyExtractor={item => item.orderId}
+          contentContainerStyle={styles.flatlistContainer}
+        />
       </View>
     </View>
   );
--- src/service/orderService.tsx
+++ src/service/orderService.tsx
@@ -14,6 +14,7 @@ export const createOrder = async (items: any, totalPrice: number) => {
     return null;
   }
 };
+
 export const getOrderById = async (id: string) => {
   try {
     const response = await appAxios.get(`/order/${id}`);
@@ -24,6 +25,7 @@ export const getOrderById = async (id: string) => {
     return null;
   }
 };
+
 export const fetchCustomerOrders = async (userId: string) => {
   try {
     const response = await appAxios.get(`/order?customerId${userId}`);
@@ -31,6 +33,25 @@ export const fetchCustomerOrders = async (userId: string) => {
     return response.data;
   } catch (error) {
     console.log('Fetch Customer Order Error', error);
+    return null;
+  }
+};
+
+export const fetchOrders = async (
+  status: string,
+  userId: string,
+  branchId: string,
+) => {
+  let uri =
+    status == 'available'
+      ? `/order?status=${status}&branchId=${branchId}`
+      : `/order?branchId=${branchId}&deliveryPartnerId=${userId}&status=delivered`;
+
+  try {
+    const response = await appAxios.get(uri);
+    return response.data;
+  } catch (error) {
+    console.log('Fetch Delivery Order Error', error);
     return null;
   }
 };
