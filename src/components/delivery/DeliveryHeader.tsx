b5ceb06cf15d49042a043e5382bc1ac80f9cd5cd
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-18 19:16:33 +0530

    DELIVERY-DASHBOARD ADDED

---
+++ src/components/delivery/DeliveryHeader.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const DeliveryHeader = () => {
+  return (
+    <View>
+      <Text>DeliveryHeader</Text>
+    </View>
+  );
+};
+
+export default DeliveryHeader;
--- src/features/delivery/DeliveryDashboard.tsx
+++ src/features/delivery/DeliveryDashboard.tsx
@@ -1,12 +1,40 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import React from 'react';
+import {Colors} from '@utils/Constants';
+import DeliveryHeader from '@components/delivery/DeliveryHeader';
+import {useAuthStore} from '@state/authStore';
 
 const DeliveryDashboard = () => {
+  const {user} = useAuthStore();
   return (
-    <View>
-      <Text>DeliveryDashboard</Text>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <DeliveryHeader name={user?.name} email={user?.email} />
+      </SafeAreaView>
+      <View style={styles.subContainer}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.primary,
+    flex: 1,
+  },
+  subContainer: {
+    backgroundColor: Colors.backgroundSecondary,
+    flex: 1,
+    padding: 6,
+  },
+  flatlistContainer: {
+    padding: 2,
+  },
+  center: {
+    flex: 1,
+    marginTop: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default DeliveryDashboard;
