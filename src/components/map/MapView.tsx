c6c17a205c9f7001b908b6e0a73cd14815c01ae4
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-20 19:40:40 +0530

    MAP-VIEW ADDED

---
+++ src/components/map/MapView.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const MapViewComponent = () => {
+  return (
+    <View>
+      <Text>MapView</Text>
+    </View>
+  );
+};
+
+export default MapViewComponent;
--- src/features/map/LiveMap.tsx
+++ src/features/map/LiveMap.tsx
@@ -1,7 +1,11 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React, {FC} from 'react';
 import {screenHeight} from '@utils/Scailing';
 import {Colors} from '@utils/Constants';
+import {useMapRefStore} from '@state/mapStore';
+import MapViewComponent from '@components/map/MapView';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {RFValue} from 'react-native-responsive-fontsize';
 
 interface LiveMapProps {
   deliveryPersonLocation: any;
@@ -18,9 +22,13 @@ const LiveMap: FC<LiveMapProps> = ({
   hasAccepted,
   hasPickedUp,
 }) => {
+  const {mapRef, setMapRef} = useMapRefStore();
   return (
     <View style={styles.container}>
-      <Text>LiveMap</Text>
+      <MapViewComponent />
+      <TouchableOpacity style={styles.fitButton}>
+        <Icon name="target" size={RFValue(14)} color={Colors.text} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -35,6 +43,21 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: Colors.border,
     position: 'relative',
+  },
+  fitButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    padding: 5,
+    backgroundColor: '#fff',
+    borderWidth: 0.8,
+    borderColor: Colors.border,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    shadowColor: 'black',
+    elevation: 5,
+    borderRadius: 35,
   },
 });
 
